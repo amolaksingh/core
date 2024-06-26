37e39219e0928f35260d3afc8a2fe0d29069d426
Amolak Singh <amolaksingh@codebrewinnovations.com>
2024-06-26 11:43:26 +0530

    View data binding issue fix

--- chartview/src/main/java/com/chartview/ChartMinimal.kt
+++ chartview/src/main/java/com/chartview/ChartMinimal.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
-import io.horizontalsystems.chartview.databinding.ViewChartMinimalBinding
+import com.chartview.databinding.ViewChartMinimalBinding
 import com.chartview.models.ChartConfig
 
 class ChartMinimal @JvmOverloads constructor(
