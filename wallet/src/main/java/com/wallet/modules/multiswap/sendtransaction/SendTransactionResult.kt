3e4a77689d8a657c61c973a358ce563a87eb08d2
Amolak Singh <amolaksingh@codebrewinnovations.com>
2024-06-26 16:15:27 +0530

    Minor fix in core

--- wallet/src/main/java/com/wallet/modules/multiswap/sendtransaction/SendTransactionResult.kt
+++
@@ -1,7 +0,0 @@
-package io.horizontalsystems.bankwallet.modules.multiswap.sendtransaction
-
-import io.horizontalsystems.ethereumkit.models.FullTransaction
-
-sealed class SendTransactionResult {
-    data class Evm(val fullTransaction: FullTransaction) : SendTransactionResult()
-}
