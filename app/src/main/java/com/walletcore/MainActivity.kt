01607c279fe4e1c84da19cc13bc6e8b8886b13cb
Amolak Singh <amolaksingh@codebrewinnovations.com>
2024-06-27 22:43:35 +0530

    increase xmx

--- app/src/main/java/com/walletcore/MainActivity.kt
+++ app/src/main/java/com/walletcore/MainActivity.kt
@@ -3,16 +3,12 @@ package com.walletcore
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.ReportFragment.Companion.reportFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val abc = com.wallet.MainActivity().onCreate()
     }
 
 }
